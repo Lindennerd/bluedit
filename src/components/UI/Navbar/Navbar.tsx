62f2aa26ace443625d840138633da8478990583e
lindennerd <lindennerd@gmail.com>
2022-09-29 07:50:46 -0300

    changing border color on dark mode for navbar

--- src/components/UI/Navbar/Navbar.tsx
+++ src/components/UI/Navbar/Navbar.tsx
@@ -6,7 +6,7 @@ import { FaReddit } from "react-icons/fa";
 export function Navbar() {
   const { status } = useSession();
   return (
-    <nav className="flex justify-between items-center fixed top-0 px-4 py-2 h-12 border-b w-full">
+    <nav className="flex justify-between items-center fixed top-0 px-4 py-2 h-12 border-b w-full dark:border-b-slate-600">
       <div
         className="font-semibold flex items-center gap-2 border-0 px-2 py-1 rounded-full
         hover:bg-slate-100 dark:hover:bg-slate-600 cursor-pointer"
