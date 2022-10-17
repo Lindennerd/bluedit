1ba3f2c84d6fb4e903932fff1194458a74581389
lindennerd <lindennerd@gmail.com>
2022-10-17 06:46:32 -0300

    post form

--- src/components/Post/Form.tsx
+++ src/components/Post/Form.tsx
@@ -2,50 +2,71 @@ import { TextInput } from "../UI/Form/TextInput";
 import { InputImage } from "../UI/Form/InputImage";
 import { FormGroup } from "../UI/Form/FormGroup";
 import { Textarea } from "../UI/Form/Textarea";
-import QueryCellComponent from "../Utils/QueryCell";
-import { inferQueryOutput } from "../../utils/trpc";
+import { trpc } from "../../utils/trpc";
+import { InputSelect } from "../UI/Form/InputSelect";
+import { useState } from "react";
+import { CommunityInputSchema } from "../../schemas/community.schema";
+import { PostInputSchema } from "../../schemas/post.schema";
 
 export function PostForm() {
+  const {
+    data: communities,
+    isLoading,
+    error,
+  } = trpc.useQuery(["community.findUsersCommunity"], {
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+  });
+
+  const [post, setPost] = useState<PostInputSchema>({} as PostInputSchema);
+
   return (
     <div>
       <form className="w-full flex flex-col gap-2">
-        <div className="w-full">
-          <QueryCellComponent
-            query="community.findUsersCommunity"
-            success={(
-              communities: inferQueryOutput<"community.findUsersCommunity">
-            ) => (
-              <>
-                {communities.map((community) => (
-                  <div key={community.id}>{community.name}</div>
-                ))}
-              </>
-            )}
-          />
-
-          <QueryCellComponent
-            query="community.findBySlug"
-            parameters={{ name: "teste" }}
-            success={(community: inferQueryOutput<"community.findBySlug">) => (
-              <div>{community?.name}</div>
-            )}
-          />
-        </div>
         <FormGroup>
-          <TextInput label="Title" />
+          {isLoading && <div>loading communities</div>}
+          {communities && (
+            <InputSelect
+              label="Communities"
+              onChange={(option) =>
+                setPost((post) => ({ ...post, communityId: option.value }))
+              }
+              options={communities?.map((comm) => ({
+                name: comm.name,
+                value: comm.id,
+              }))}
+            />
+          )}
         </FormGroup>
         <FormGroup>
-          <Textarea label="Content" />
+          <TextInput
+            label="Title"
+            onChange={(e) =>
+              setPost((post) => ({ ...post, title: e.target.value }))
+            }
+          />
+        </FormGroup>
+        <FormGroup>
+          <Textarea
+            label="Content"
+            onChange={(e) =>
+              setPost((post) => ({ ...post, content: e.target.value }))
+            }
+          />
         </FormGroup>
         <FormGroup>
           <InputImage
             label="Add an image to your post"
             accept="*"
-            onChange={(e) => {}}
+            onChange={(e) =>
+              setPost((post) => ({ ...post, image: e?.item(0)!.name }))
+            }
             className="p-2 rounded-full bg-secondary_light"
           />
         </FormGroup>
         <button type="submit">Save</button>
+
+        {JSON.stringify(post)}
       </form>
     </div>
   );
--- src/components/UI/Form/InputSelect.tsx
+++ src/components/UI/Form/InputSelect.tsx
@@ -1,34 +1,121 @@
-import { Listbox, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
-import { BiCheck } from "react-icons/bi";
+import { Combobox, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useState } from "react";
+import { BiCaretDown, BiCheck } from "react-icons/bi";
 
 export type SelectOption = {
-  displayValue: string;
+  name: string;
   value: string;
 };
 
 interface InputSelectProps {
   label: string;
   options: SelectOption[];
+  onChange: (selected: SelectOption) => void;
 }
 
-export function InputSelect({ label, options }: InputSelectProps) {
+export function InputSelect({ label, options, onChange }: InputSelectProps) {
   const [selectedOption, setSelectedOption] = useState<SelectOption | null>();
+  const [query, setQuery] = useState("");
+
+  const filteredOptions =
+    query === ""
+      ? options
+      : options.filter((option) =>
+          option.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  useEffect(() => {
+    if (selectedOption) onChange(selectedOption);
+  }, [selectedOption]);
 
   return (
-    <select
-      onChange={(e) =>
-        setSelectedOption({
-          value: e.target.value,
-          displayValue: e.target.textContent!,
-        })
-      }
-    >
-      {options.map((option) => (
-        <option key={option.value} value={option.value}>
-          {option.displayValue}
-        </option>
-      ))}
-    </select>
+    <div className="">
+      <label>{label}</label>
+      <Combobox value={selectedOption} onChange={setSelectedOption}>
+        <div className="relative mt-1">
+          <div
+            className="relative w-full cursor-default overflow-hidden
+           bg-white text-left shadow-md focus:outline-none focus-visible:ring-2
+            focus-visible:ring-secondary_light focus-visible:ring-opacity-75 
+            focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm rounded-full"
+          >
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 rounded-full
+               leading-5 text-gray-900 dark:text-white focus:ring-0 outline-none"
+              //@ts-ignore
+              displayValue={(_options) =>
+                selectedOption ? selectedOption.name : options[0]?.name
+              }
+              onChange={(event) => setQuery(event.target.value)}
+            />
+            <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+              <BiCaretDown
+                className="h-5 w-5 text-gray-400"
+                aria-hidden="true"
+              />
+            </Combobox.Button>
+          </div>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options
+              className="absolute mt-1 max-h-60 w-full overflow-auto
+             rounded-md bg-white py-1 text-base shadow-lg ring-1
+              ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+            >
+              {filteredOptions.length === 0 && query !== "" ? (
+                <div
+                  className="relative cursor-default select-none py-2 
+                px-4 text-gray-700"
+                >
+                  Nothing found.
+                </div>
+              ) : (
+                filteredOptions.map((option, index) => (
+                  <Combobox.Option
+                    key={index}
+                    className={({ active }) =>
+                      `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                        active ? "bg-primary_light text-white" : "text-gray-900"
+                      }`
+                    }
+                    value={option}
+                  >
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? "font-medium" : "font-normal"
+                          }`}
+                        >
+                          {option.name}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center 
+                            pl-3 ${
+                              active ? "text-white" : "text-primary_light"
+                            }`}
+                          >
+                            <BiCheck className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option>
+                ))
+              )}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
   );
 }
